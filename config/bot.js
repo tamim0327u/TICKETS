d8c484b77c3af92301991e6110d63df1f28d9e39
tamim0327u <94356074+tamim0327u@users.noreply.github.com>
2022-02-08 23:45:02 +0600

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+module.exports = {
+    token: process.env.TOKEN,
+    prefix: process.env.PREFIX
+}
+// I have made all of this .env !
+
+/**********************************************************
+ * @INFO
+ * Bot Coded by R J | https://discord.gg/8fYUFxMtAq
+ * @INFO
+ * Work for Milanio Development | https://discord.gg/8fYUFxMtAq
+ * @INFO
+ * Please Mention Us Milanio Development, When Using This Code!
+ * @INFO
+ *********************************************************/
